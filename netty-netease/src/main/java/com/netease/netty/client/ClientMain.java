c849b2e3149dc8a18a25a9a25f9989289563c81f
sunxiaoxiao <sunxiaoxiao@corp.netease.com>
2019-08-23 15:26:06 +0800

    2019-08-23

---
+++ netty-netease/src/main/java/com/netease/netty/client/ClientMain.java
@@ -0,0 +1,12 @@
+package com.netease.netty.client;
+
+/**
+ * Description:
+ *
+ * @author sunxiaoxiao
+ * @version v1.0
+ * @date 2019-08-23
+ * @since JDK 1.8
+ */
+public class ClientMain {
+}
---
+++ netty-netease/src/main/java/com/netease/netty/server/ServerChannelFilter.java
@@ -0,0 +1,12 @@
+package com.netease.netty.server;
+
+/**
+ * Description:
+ *
+ * @author sunxiaoxiao
+ * @version v1.0
+ * @date 2019-08-23
+ * @since JDK 1.8
+ */
+public class ServerChannelFilter {
+}
